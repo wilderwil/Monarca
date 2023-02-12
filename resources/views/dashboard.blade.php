85beac4f05eaff1814b74887593235edff6754ba
Wilder Wilches <wilderwil@gmail.com>
2023-02-12 11:46:47 -0400

    Initial commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,25 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+
+
+@section('content_header')
+
+    <h1>SGE</h1>
+@stop
+
+@section('content')
+    <div>
+        <p>Laravel plantilla Inicial</p>
+        <ul>
+            <li>Laravel</li>
+            <li>Breeze & Blade</li>
+            <li>Spatie Laravel Permission</li>
+            <li>Admin Lte</li>
+        </ul>
+
+    </div>
+
+
+@stop
